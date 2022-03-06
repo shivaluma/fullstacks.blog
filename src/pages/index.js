cf6e49f575a261011d57f76fee0317da31743729
shivaluma <shivaluma@gmail.com>
2022-03-06 20:35:25 +0700

    Update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,6 @@ const BlogIndex = ({ data, location }) => {
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
         <p>
           No blog posts found. Add markdown posts to "content/blog" (or the
           directory you specified for the "gatsby-source-filesystem" plugin in
@@ -24,7 +23,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout className="flex" location={location} title={siteTitle}>
-      <Seo title="All posts" />
+      <Seo title="Thanh's Blog" />
       <ol className="list-style-none">
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
